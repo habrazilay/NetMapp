8eaf10f4d252cc20c33b425acc5d1e7a223a40c7
Daniel Schmidt <dsch87@gmail.com>
2016-08-28 19:53:43 +0300

    cabinets quantity added description room added try to add header to all pages with inculde php function

--- production/test.php
+++ production/test.php
@@ -1,1 +1,21 @@
-<?php include("~/production/header.html");?>
+<?php 
+    include('/NetMapp/production/header.html');
+?>
+
+<!--<html>
+<head>
+<title>Include Common Files</title>
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body>
+<div id="header"></div><br/>
+Main Content
+</div><br />
+<div id="footer"></div>
+<script>
+$("#header").load("header.html");
+$("#sidebar_menu").load("sidebar_menu.html");
+$("#footer").load("footer.html");
+</script>
+</body>
+</html>-->
