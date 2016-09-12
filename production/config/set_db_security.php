0d8a44ad67b549e6d7362ae7f0796356eade0ff5
venturaniv <niv.ventt@gmail.com>
2016-09-13 01:07:52 +0300

    configuring parameters for mysqli connection.
    config/set_mysql_server.php sets ip,user,pass of the database.
    
    config/set_db_security.php sets the security schema of the database.
    
    production/classes/Login.php has an example of mysqli connection (line 58), and query execution (line 73).

---
+++ production/config/set_db_security.php
@@ -0,0 +1,9 @@
+<?php 
+
+/*
+ * Configuration for: Database Server and Schema
+ * DB_NAME: name of the database schema. please note: database schema and database table are not the same thing.
+ */
+require_once("config/set_mysql_server.php");
+define("DB_NAME", "security");
+ 
