4fad16ff2ff6c6b4bda04fa6f0e640844335e112
venturaniv <niv.ventt@gmail.com>
2016-09-13 02:17:56 +0300

    this is an example of using user login session, redirecting guests to login and registration page.

---
+++ production/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// load the login class
+require_once("classes/Login.php");
+
+// create a login object. when this object is created, it will do all login/logout stuff automatically
+// so this single line handles the entire login process. in consequence, you can simply ...
+$login = new Login();
+
+
+// ... ask if we are logged in here:
+if ($login->isUserLoggedIn() == true) {
+    // the user is logged in.
+    include("index.html");
+
+} else {
+    // the user is not logged in.
+    include("login.php");
+}
