7c84aaf2fa3bf4c22f4309ade00bd45e7bec7989
Niv Ventura <niv.ventt@gmail.com>
2016-11-08 22:09:04 +0200

    added user_id to login session

--- production/classes/Login.php
+++ production/classes/Login.php
@@ -70,7 +70,7 @@ class Login
 
                 // database query, getting all the info of the selected user (allows login via email address in the
                 // username field)
-                $sql = "SELECT user_name, user_email, user_password_hash
+                $sql = "SELECT user_name, user_email, user_password_hash, id
                         FROM users
                         WHERE user_name = '" . $user_name . "' OR user_email = '" . $user_name . "';";
                 $result_of_login_check = $this->db_connection->query($sql);
@@ -89,6 +89,7 @@ class Login
                         $_SESSION['user_name'] = $result_row->user_name;
                         $_SESSION['user_email'] = $result_row->user_email;
                         $_SESSION['user_login_status'] = 1;
+                        $_SESSION['user_id'] = $result_row->id;
 
                     } else {
                         $this->errors[] = "Wrong password. Try again.";
