7bf878227c2940de260de903775e56660931e97b
Niv Ventura <niv.ventt@gmail.com>
2017-03-09 15:19:17 +0200

    added db query data verification

--- production/config/get_room.php
+++ production/config/get_room.php
@@ -2,7 +2,7 @@
 require_once("dbcontroller.php");
 require_once("set_mysql_server.php");
 $db_handle = new DBController(DB_SCHEMA_PROJECT);
-if(!empty($_POST["sid"])) {
+if(!empty($_POST["sid"]) AND is_numeric($_POST["sid"])) {
 	$query ="SELECT * FROM rooms WHERE sid = '" . $_POST["sid"] . "'";
 	$results = $db_handle->runQuery($query,DB_SCHEMA_PROJECT);
 ?>
