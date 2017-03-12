19087bcf75541d0091e1f0a6661fc303ced644f0
Niv Ventura <niv.ventt@gmail.com>
2017-03-12 20:00:29 +0200

    added insertion querys

--- database/Create_Database.bat
+++ database/Create_Database.bat
@@ -3,3 +3,6 @@ cmd /c "mysql -uroot < .\Creation-Querys\securityDBCreation.sql"
 cmd /c "mysql -uroot < .\Creation-Querys\projectDBCreation.sql"
 cmd /c "mysql -uroot < .\Creation-Querys\projectSettingsDBCreation.sql"
 cmd /c "mysql -uroot < .\Creation-Querys\mappingDBCreation.sql"
+
+cmd /c "mysql -uroot < .\Insertion-Querys\baseDBInsertion.sql"
+cmd /c "mysql -uroot < .\Insertion-Querys\projectDBInsertion.sql"
