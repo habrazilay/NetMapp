255fa7cc226a3df36b31c826cbeb20e5d975a408
Niv Ventura <niv.ventt@gmail.com>
2017-03-12 19:54:05 +0200

    changed get_room.php SQL execution to secured prepared execution.

--- production/config/get_room.php
+++ production/config/get_room.php
@@ -2,14 +2,15 @@
 require_once("dbcontroller.php");
 require_once("set_mysql_server.php");
 $db_handle = new DBController(DB_SCHEMA_PROJECT);
+
 if(!empty($_POST["sid"]) AND is_numeric($_POST["sid"])) {
-	$query ="SELECT * FROM rooms WHERE sid = '" . $_POST["sid"] . "'";
-	$results = $db_handle->runQuery($query,DB_SCHEMA_PROJECT);
+	$query ="SELECT * FROM rooms WHERE sid = ?";
+	$results = $db_handle->prepareAndRunQuery($query,DB_SCHEMA_PROJECT,'i',$_POST["sid"]);
 ?>
 <?php
 	foreach($results as $room) {
 ?>
-	<option value="<?php echo $room["id"]; ?>"><?php echo $room["name"]; ?></option>
+	<option value="<?php echo $room["id"]; ?>"><?php echo $room["name"]; echo $room["id"]; ?></option>
 <?php
 	}
 }
