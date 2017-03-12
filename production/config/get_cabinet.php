7eb7a1f2abf3930180545e82932476975fac8741
Niv Ventura <niv.ventt@gmail.com>
2017-03-12 20:06:47 +0200

    changed get_cabinet.php SQL execution to secured prepared execution.

--- production/config/get_cabinet.php
+++ production/config/get_cabinet.php
@@ -2,11 +2,13 @@
 require_once("dbcontroller.php");
 require_once("set_mysql_server.php");
 $db_handle = new DBController(DB_SCHEMA_MAP);
+var_dump($_POST);
 if(!empty($_POST["rid"])) {
-    $query ="SELECT * FROM cabinets WHERE rid = '" . $_POST["rid"] . "'";
-    $results = $db_handle->runQuery($query,DB_SCHEMA_MAP);
+	$query ="SELECT * FROM cabinets WHERE rid = ?";
+	$results = $db_handle->prepareAndRunQuery($query,DB_SCHEMA_MAP,'i',$_POST["rid"]);
 ?>
 <?php
+	if(empty($results)) return;
     foreach($results as $cabinet) {
 ?>
     <option value="<?php echo $cabinet["id"]; ?>"><?php echo $cabinet["name"]; ?></option>
