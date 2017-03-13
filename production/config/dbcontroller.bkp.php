03f25288c2f0d4f37f7cb278140c6b729bb7df9e
Niv Ventura <niv.ventt@gmail.com>
2017-03-13 16:50:49 +0200

    changed behavior of dbcontroller,
    added last query affected rowsfunction.

--- production/config/dbcontroller.bkp.php
+++ production/config/dbcontroller.bkp.php
@@ -1,32 +1,27 @@
 <?php
-
 class DBController {   
 	
-	private $conn;
-	private $db;
-	private $affectedRows;
-	
 	function __construct($db) {
-		$this->db = $db;
-		$this->connectDB();
-		if(!empty($this->conn)) {
-			$this->selectDB();
+		$conn = $this->connectDB($db);
+		if(!empty($conn)) {
+			$this->selectDB($conn,$db);
 		}
 	}
 	
-	function connectDB() {
-	    require_once($_SERVER['DOCUMENT_ROOT']."/NetMapp/production/config/set_mysql_server.php");
-		$this->conn = new mysqli(DB_HOST,DB_USER,DB_PASS, $this->db);
+	function connectDB($db) {
+	    require_once("set_mysql_server.php");
+		$conn = new mysqli(DB_HOST,DB_USER,DB_PASS, $db);
+		return $conn;
 	}
 	
-	function selectDB() {
-	    require_once($_SERVER['DOCUMENT_ROOT']."/NetMapp/production/config/set_mysql_server.php");
-		mysqli_select_db($this->conn, $this->db)or die("cannot select DB");
+	function selectDB($conn, $db) {
+	    require_once("set_mysql_server.php");
+		mysqli_select_db($conn, $db)or die("cannot select DB");
 	}
 
 	function runQuery($query,$db) {
-	    //$this->conn = new mysqli(DB_HOST,DB_USER,DB_PASS,$this->db);
-		$result = mysqli_query($this->conn, $query);
+	    $conn = new mysqli(DB_HOST,DB_USER,DB_PASS,$db);
+		$result = mysqli_query($conn, $query);
 		while($row=$result->fetch_assoc()) {
 			$resultset[] = $row;
 		}		
@@ -41,7 +36,7 @@ class DBController {
 	}
 	
 	/* This function executes a sql query safely using prepared statements and bind parameterssafe sql execution.
-	 * use: prepareAndRunQuery($Prepared_Query,$this->db,$format_string,$arg1,$arg2,...);
+	 * use: prepareAndRunQuery($Prepared_Query,$db,$format_string,$arg1,$arg2,...);
 	 */
 	function prepareAndRunQuery($query,$db,$formatStr,...$args) {
 		$errors = array();
@@ -49,9 +44,9 @@ class DBController {
 		if ( (substr_count($query, '?') != $countArgs) || (strlen($formatStr) != $countArgs) )
 			$errors[] = "Mismatching number of args: amount of '?' in query, count of format string and count of args must be equal."; 
 		
-		//require_once("set_mysql_server.php");
-		//$this->conn = new mysqli(DB_HOST,DB_USER,DB_PASS,$this->db);
-		if(!$stmt = $this->conn->prepare($query))
+		require_once("set_mysql_server.php");
+		$conn = new mysqli(DB_HOST,DB_USER,DB_PASS,$db);
+		if(!$stmt = $conn->prepare($query))
 			$errors[] = "Failed to prepare query.";
 		if(!$stmt->bind_param($formatStr, ...$args))
 			$errors[] = "Failed to bind parameters into query.";
@@ -59,7 +54,6 @@ class DBController {
 			$errors[] = "Failed to execute query.";
 			$errors[] = mysqli_stmt_error($stmt);
 		}
-		$this->affectedRows = mysqli_affected_rows($this->conn);
 		
 		if(!empty($errors)){
 			echo print_r($errors);
@@ -76,11 +70,6 @@ class DBController {
 		}
 		
 	}
-	
-	function affectedRows() {
-		return $this->affectedRows;
-	}
 			
 }
-
 ?>
