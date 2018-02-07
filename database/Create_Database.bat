59c838ccfb7b4d0595299be2153a13fed395c40b
Daniel Schmidt <dsch87@gmail.com>
2018-02-07 12:10:49 +0200

    Data base creation and delete updated to work with right click "Run as administrator"

--- database/Create_Database.bat
+++ database/Create_Database.bat
@@ -1,3 +1,5 @@
+cd %~dp0
+
 cmd /c "mysql -uroot < .\Creation-Querys\baseDBCreation.sql"
 cmd /c "mysql -uroot < .\Creation-Querys\securityDBCreation.sql"
 cmd /c "mysql -uroot < .\Creation-Querys\projectDBCreation.sql"
@@ -6,3 +8,5 @@ cmd /c "mysql -uroot < .\Creation-Querys\mappingDBCreation.sql"
 
 cmd /c "mysql -uroot < .\Insertion-Querys\baseDBInsertion.sql"
 cmd /c "mysql -uroot < .\Insertion-Querys\projectDBInsertion.sql"
+
+pause 
--- database/Delete_Database.bat
+++ database/Delete_Database.bat
@@ -1,1 +1,5 @@
+cd %~dp0
+
 cmd /c "mysql -uroot < .\Delete_Database.sql"
+
+pause
