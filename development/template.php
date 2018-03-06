ae7bf2c22187c21bd5a933ce83b51397eb5bca92
Daniel Schmidt <dsch87@gmail.com>
2018-03-06 19:55:24 +0200

    Template update.

--- development/template.php
+++ development/template.php
@@ -1,11 +1,39 @@
-<!--<?php include($_SERVER['DOCUMENT_ROOT']."/NetMapp/development/loginVerify.php"); ?>-->
-<?php include($_SERVER['DOCUMENT_ROOT']."/NetMapp/development/header.html"); ?>
-<?php include($_SERVER['DOCUMENT_ROOT']."/NetMapp/development/sidebar_menu.html"); ?>
-<?php include($_SERVER['DOCUMENT_ROOT']."/NetMapp/development/menu_footer.html"); ?>
-<?php include($_SERVER['DOCUMENT_ROOT']."/NetMapp/development/top_navigation.html"); ?>
+<?php include($_SERVER['DOCUMENT_ROOT']."/NetMapp/development/loginVerify.php"); ?>
+<?php 
+include($_SERVER['DOCUMENT_ROOT']."/NetMapp/development/header.html");
+include($_SERVER['DOCUMENT_ROOT']."/NetMapp/development/sidebar_menu.html");
+include($_SERVER['DOCUMENT_ROOT']."/NetMapp/development/menu_footer.html");
+include($_SERVER['DOCUMENT_ROOT']."/NetMapp/development/top_navigation.html");
+?>
 
-<!-- /page content -->
 
-<!-- /page content -->
+
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Page Name</h3>
+              </div>
+				</div>
+                          
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                
+                
+                <!-- /page content -->
+                
+                
+                <!-- /page content -->
+                                         
+                      
+                        </div>
+                      </div>
+                    </form>             
+            </div>
+          </div>
+        </div>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/NetMapp/development/footer.html"); ?>
